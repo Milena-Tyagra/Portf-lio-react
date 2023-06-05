87036aec20d18ca2f715d05a9433875f06607ccf
Milena <milenatyagra.sc@gmail.com>
2023-06-04 22:13:28 -0300

    Atualizando rotas

--- src/service/api.js
+++ src/service/api.js
@@ -1,21 +1,21 @@
 import instanciaAxios from "./axiosInstance"
 
 export async function listarCertificacoes() {
-  const response = await instanciaAxios.get("/certificacoes/")
+  const response = await instanciaAxios.get("/api/certificacoes/")
   return response.data
 }
 
 export async function listarExperiencias() {
-  const response = await instanciaAxios.get("/experiencias/") 
+  const response = await instanciaAxios.get("api/experiencias/") 
   return response.data
 }
 
 export async function buscarInformacoes() {
-  const response = await instanciaAxios.get("/informacoes/1")
+  const response = await instanciaAxios.get("/api/informacoes")
   return response.data
 }
 
 export async function listarProjetos() {
-  const response = await instanciaAxios.get("/projetos/")
+  const response = await instanciaAxios.get("api/portfolio/")
   return response.data
 }
